d768234c8a5838f08bce41f7727cb480622af871
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2017-07-24 04:57:33 -0400

    initial commit

--- apps/first_app/urls.py
+++ apps/first_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 from . import views
 urlpatterns = [
         url(r'^$', views.index),
-        url(r'^/first_app/all.json$', views.all_json),
+        url(r'^first_app/all.json$', views.all_json),
         url(r'^first_app/all.html$', views.all_html),
         url(r'^first_app/find$', views.find),
         url(r'^first_app/create$', views.create),
